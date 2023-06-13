f1da98ea757a6c076a53fa5efce42f410aa48097
Rouuuben <ruben.pereiralopes@heig-vd.ch>
2023-06-13 17:22:06 +0200

    fix name

--- src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
@@ -10,7 +10,7 @@ public class OrdersWriterTest {
 
     @BeforeEach
     public void SetupOneOrder() {
-        orders.AddOrder(order111);
+        orders.addOrder(order111);
     }
 
     @Test
@@ -26,7 +26,7 @@ public class OrdersWriterTest {
 
     @Test
     public void TwoOrders() {
-        orders.AddOrder(new Order(222));
+        orders.addOrder(new Order(222));
         String order111Json = JsonOrder111WithProduct("");
         String order222Json = "{\"id\": 222, \"products\": []}";
         assertEquals("{\"orders\": [" + order111Json + ", " + order222Json + "]}", new OrdersWriter(orders).getContents());
@@ -34,14 +34,14 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProduct() {
-        order111.AddProduct(new Product("Shirt", 1, 3, 2.99, "TWD"));
+        order111.addProduct(new Product("Shirt", 1, 3, 2.99, "TWD"));
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
     }
 
     @Test
     public void OneOrderWithOneProductNoSize() {
-        order111.AddProduct(new Product("Pot", 2, -1, 16.50, "SGD"));
+        order111.addProduct(new Product("Pot", 2, -1, 16.50, "SGD"));
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
     }
