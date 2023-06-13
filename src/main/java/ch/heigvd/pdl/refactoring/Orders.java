30a9543c6c69f926ead8fe3c162337527ac00a28
Rouuuben <ruben.pereiralopes@heig-vd.ch>
2023-06-13 17:55:40 +0200

    'code smell' identifié, renommage de la variable 'orders' en 'ordersList', Orders.java impacté

--- src/main/java/ch/heigvd/pdl/refactoring/Orders.java
+++ src/main/java/ch/heigvd/pdl/refactoring/Orders.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class Orders {
 
-    private List<Order> orders = new ArrayList<Order>();
+    private List<Order> ordersList = new ArrayList<Order>();
 
     public void AddOrder(Order order) {
-        orders.add(order);
+        ordersList.add(order);
     }
 
     public int getOrdersCount() {
-        return orders.size();
+        return ordersList.size();
     }
 
     public Order getOrder(int i) {
-        return orders.get(i);
+        return ordersList.get(i);
     }
 
 }
